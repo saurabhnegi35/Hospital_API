23cdaebdf4acdc87c082ccdbf7d1239b188e61db
SaurabhNegi35 <69843862+SaurabhNegi35@users.noreply.github.com>
2023-02-28 19:12:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const port = process.env.PORT || 2000;
 const db = require('./config/mongoose');
+const routes = require('./routes/index');
+const passport = require('passport');
+const passportJWT = require('./config/passport-jwt-strategy');
 
 const app = express();
 
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
 
-app.listen(port, ()=> {
-    console.log(`Server is Up and Running at Port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is Up and Running at Port ${port}`);
 });
